364f90bcc7af5e7ba65e462c7fcc2513a73c12ef
iantaylor-NOAA <ian.taylor@noaa.gov>
2025-01-31 12:31:06 -0800

    flag years that we exclude from the survey in plot

--- R/survey_summary_US_West_Coast.R
+++ R/survey_summary_US_West_Coast.R
@@ -30,24 +30,39 @@ catch$yday <- lubridate::yday(catch$date)
 # 2nd try for day within each year by making the year be the same for all entries
 catch$yday2 <- catch$date
 lubridate::year(catch$yday2) <- 1900
+# flag years that we typically use
+catch <- catch |> 
+  dplyr::mutate(
+    exclude = dplyr::case_when(
+      survey_name == "Triennial" & lubridate::year(date) < 1980 ~ TRUE,
+      survey_name == "AFSC Slope" & lubridate::year(date) < 1997 ~ TRUE,
+      TRUE ~ FALSE # all other surveys/years
+    )
+  )
+
+# reorder points so that the colors mix instead of one masking the other
+catch <- catch[sample(1:nrow(catch)),]
 
 # make plots 
 library(ggplot2)
 g1 <- ggplot(catch, aes(date, yday2)) +
-  geom_point(aes(color = survey_name), alpha = 0.3) +
+  geom_point(aes(color = survey_name, shape = exclude), fill = "grey", alpha = 0.3) +
   labs(x = "Date", y = "Day within year", color = "") + 
   scale_y_date(date_breaks = "1 month", date_labels = "%b") + 
+  scale_shape_manual(values = c(19, 21)) + 
   guides(colour = guide_legend(override.aes = list(alpha = 1)))
 
 g2 <- ggplot(catch, aes(date, Latitude_dd)) +
-  geom_point(aes(color = survey_name), alpha = 0.3) +
+  geom_point(aes(color = survey_name, shape = exclude), fill = "grey", alpha = 0.3) +
   labs(x = "Date", y = "Latitude (°N)", color = "") + 
+  scale_shape_manual(values = c(19, 21)) + 
   guides(colour = guide_legend(override.aes = list(alpha = 1)))
 
 g3 <- ggplot(catch, aes(date, Depth_m)) +
-  geom_point(aes(color = survey_name), alpha = 0.3) + 
+  geom_point(aes(color = survey_name, shape = exclude), fill = "grey", alpha = 0.3) + 
   scale_y_continuous(transform = "log2") +
   labs(x = "Date", y = "Depth (m)", color = "") + 
+  scale_shape_manual(values = c(19, 21)) + 
   guides(colour = guide_legend(override.aes = list(alpha = 1)))
 
 # combine plots and save results
